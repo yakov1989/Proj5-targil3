d2ba3eb431ea447ee48535832d741fcc66804b5d
yakobino1989 <yakobino1989@gmail.com>
2018-11-06 10:42:49 +0200

    Add project files.

---
+++ Proj5-targil3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj5_targil3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
